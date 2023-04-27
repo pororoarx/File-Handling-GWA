0a46afedf0b07ee9e305faba77ac891b880d5ad3
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-27 10:48:44 +0800

    Define a function; open students_gwa.txt; create empty dictionary

--- file_handling_gwa.py
+++ file_handling_gwa.py
@@ -1,8 +1,11 @@
 # Start
 
 # define a function called students_gwa_reader
+def student_gwa_reader():
     # open students_gwa.txt (read)
+    with open("students_gwa.txt", "r") as file_1:
         # create empty dictionary
+        student_gwa = {}
         # loop through each line in students_gwa.txt
             # split the line into name and gwa and convert gwa to float
         # find the key with the lowest value (lowest value = highest gwa) in student_gwa dictionary
