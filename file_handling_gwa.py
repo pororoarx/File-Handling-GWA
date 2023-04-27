ac76fdd60b8e2a01ed8e75cdcf369ba252243a7e
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-27 10:32:38 +0800

    Pseudocode

---
+++ file_handling_gwa.py
@@ -0,0 +1,13 @@
+# Start
+
+# define a function called students_gwa_reader
+    # open students_gwa.txt (read)
+        # create empty dictionary
+        # loop through each line in students_gwa.txt
+            # split the line into name and gwa and convert gwa to float
+        # find the key with the lowest value (lowest value = highest gwa) in student_gwa dictionary
+        # get the key with the lowest value in student_gwa dictionary
+    # print the result
+
+
+# call the function
